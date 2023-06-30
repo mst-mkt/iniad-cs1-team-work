faa2b4fed208f61b23f4b7c24ee499a6a39baaf4
mst-mkt <s1f102303057@iniad.org>
2023-06-30 16:17:06 +0900

    add some recommend

--- script/main.js
+++ script/main.js
@@ -2,3 +2,8 @@ const toggleSidemenu = () => {
     const sidemenu = document.getElementsByClassName('sidemenu')[0];
     sidemenu.classList.toggle('sidemenu--active');
 }
+
+const moveSidemenu = () => {
+    const slideshow = document.getElementsByClassName('slideshow')[0];
+    const width = slideshow.offsetWidth;
+}
