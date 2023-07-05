b2ebb8c456dc974f2b6b5336ccb2dfb4d273aab3
mst-mkt <s1f102303057@iniad.org>
2023-07-05 19:55:26 +0900

    add: slideshow scripts and styles

--- script/main.js
+++ script/main.js
@@ -1,9 +1,47 @@
 const toggleSidemenu = () => {
-    const sidemenu = document.getElementsByClassName('sidemenu')[0];
-    sidemenu.classList.toggle('sidemenu--active');
-}
+    const sidemenu = document.getElementsByClassName("sidemenu")[0];
+    sidemenu.classList.toggle("sidemenu--active");
+};
 
-const moveSidemenu = () => {
-    const slideshow = document.getElementsByClassName('slideshow')[0];
-    const width = slideshow.offsetWidth;
-}
+const slideShow = document.getElementsByClassName("slideshow__container")[0];
+
+const setSlideDot = () => {
+    const width = slideShow.offsetWidth;
+    const scroll = slideShow.scrollLeft;
+    const index = Math.round(scroll / width);
+    const slideDots = document.getElementsByClassName("slideshow__dot");
+    Array.from(slideDots).map((dot) =>
+        dot.classList.remove("slideshow__dot--active")
+    );
+    slideDots[index].classList.add("slideshow__dot--active");
+};
+
+const slideRight = () => {
+    const width = slideShow.offsetWidth;
+    const scroll = slideShow.scrollLeft;
+    const maxScroll = slideShow.scrollWidth - width;
+    if (scroll >= maxScroll - 5) {
+        slideShow.scrollLeft = 0;
+    } else {
+        slideShow.scrollLeft += width;
+    }
+};
+
+const slideLeft = () => {
+    const width = slideShow.offsetWidth;
+    const scroll = slideShow.scrollLeft;
+    const maxScroll = slideShow.scrollWidth - width;
+    if (scroll === 0) {
+        slideShow.scrollLeft = maxScroll;
+    } else {
+        slideShow.scrollLeft -= width;
+    }
+};
+
+let autoSlide = setInterval(slideRight, 3000);
+const resetAutoSlide = () => {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(slideRight, 3000);
+};
+
+slideShow.addEventListener("scroll", setSlideDot);
