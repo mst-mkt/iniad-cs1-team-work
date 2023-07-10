b671f0e1df03a3e42618a6959641a9841913220e
mst-mkt <s1f102303057@iniad.org>
2023-07-10 14:14:29 +0900

    style修正、追加

--- script/main.js
+++ script/main.js
@@ -1,49 +1,10 @@
-const toggleSidemenu = () => {
-    const sidemenu = document.getElementsByClassName("sidemenu")[0];
-    const button = document.getElementsByClassName("header__button")[0];
-    sidemenu.classList.toggle("sidemenu--active");
-    button.classList.toggle("header__button--active");
+const loading = document.getElementsByClassName("loading")[0];
+(() => {
+    loading.classList.add("loading--in");
+})();
+window.onload = () => {
+    setTimeout(() => {
+        loading.classList.add("loading--outed");
+        loading.classList.remove("loading--in");
+    }, 1000);
 };
-
-const slideShow = document.getElementsByClassName("slideshow__container")[0];
-
-const setSlideDot = () => {
-    const width = slideShow.offsetWidth;
-    const scroll = slideShow.scrollLeft;
-    const index = Math.round(scroll / width);
-    const slideDots = document.getElementsByClassName("slideshow__dot");
-    Array.from(slideDots).map((dot) =>
-        dot.classList.remove("slideshow__dot--active")
-    );
-    slideDots[index].classList.add("slideshow__dot--active");
-};
-
-const slideRight = () => {
-    const width = slideShow.offsetWidth;
-    const scroll = slideShow.scrollLeft;
-    const maxScroll = slideShow.scrollWidth - width;
-    if (scroll >= maxScroll - 5) {
-        slideShow.scrollLeft = 0;
-    } else {
-        slideShow.scrollLeft += width;
-    }
-};
-
-const slideLeft = () => {
-    const width = slideShow.offsetWidth;
-    const scroll = slideShow.scrollLeft;
-    const maxScroll = slideShow.scrollWidth - width;
-    if (scroll === 0) {
-        slideShow.scrollLeft = maxScroll;
-    } else {
-        slideShow.scrollLeft -= width;
-    }
-};
-
-let autoSlide = setInterval(slideRight, 3000);
-const resetAutoSlide = () => {
-    clearInterval(autoSlide);
-    autoSlide = setInterval(slideRight, 3000);
-};
-
-slideShow.addEventListener("scroll", setSlideDot);
